cb5e3a5c65f4fb2b1805c4c48915e241fd557b65
Vinicius-Wagner <viniwagner16@gmail.com>
2024-06-22 17:02:25 -0400

    feat: adicionando a tela de gerenciar tarefas

---
+++ app/src/main/java/com/example/simpletodolist/activities/GerenciarTarefaActivity.java
@@ -0,0 +1,15 @@
+package com.example.simpletodolist.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.simpletodolist.R;
+
+public class GerenciarTarefaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerenciartarefa);
+    }
+}
--- app/src/main/java/com/example/simpletodolist/activities/TelaPrincipal.java
+++ app/src/main/java/com/example/simpletodolist/activities/TelaPrincipal.java
@@ -1,6 +1,9 @@
 package com.example.simpletodolist.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,14 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telaprincipal);
+
+        Button buttonGerenciarTarefa = findViewById(R.id.buttonGerenciarTarefa);
+        buttonGerenciarTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaPrincipal.this, GerenciarTarefaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
